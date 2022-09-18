c64efb8c75788c565423baf0746f238d126936bc
ParasBhardava <84320506+ParasBhardava@users.noreply.github.com>
2022-09-18 18:55:39 +0530

    Add files via upload

---
+++ Lab9/lab9_tutorial2/lib/main.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter/material.dart';
+// import 'package:lab9_tutorial2/pages/home.dart';
+//
+// void main() => runApp(MaterialApp(
+//       home: Home(),
+//     ));
+
+import 'package:flutter/material.dart';
+import 'package:lab9_tutorial2/pages/choose_location.dart';
+import 'package:lab9_tutorial2/pages/home.dart';
+import 'package:lab9_tutorial2/pages/loading.dart';
+
+void main() => runApp(MaterialApp(initialRoute: '/home', routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }));
